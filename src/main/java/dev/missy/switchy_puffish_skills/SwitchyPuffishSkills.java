3ebcf81c37cfb61d6ad81d779ec3890cf1c782cb
mise_png <semihoficial@gmail.com>
2025-11-04 06:05:34 +0200

    >.<

--- src/main/java/dev/missy/switchy_puffish_skills/SwitchyPuffishSkills.java
+++ src/main/java/dev/missy/switchy_puffish_skills/SwitchyPuffishSkills.java
@@ -1,10 +1,12 @@
 package dev.missy.switchy_puffish_skills;
 
+import dev.missy.switchy_puffish_skills.modules.PufferfishSkillsModule;
 import net.fabricmc.api.ModInitializer;
 
 public class SwitchyPuffishSkills implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        new PufferfishSkillsModule().onInitialize();
     }
 }
--- src/main/java/dev/missy/switchy_puffish_skills/modules/PufferfishSkillsModuleData.java
+++ src/main/java/dev/missy/switchy_puffish_skills/modules/PufferfishSkillsModuleData.java
@@ -1,4 +1,23 @@
 package dev.missy.switchy_puffish_skills.modules;
 
-public class PufferfishSkillsModuleData {
+import folk.sisby.switchy.api.SwitchySerializable;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.Identifier;
+
+public class PufferfishSkillsModuleData implements SwitchySerializable {
+    public static final String KEY_SKILLS_DATA = "skills_data";
+    protected static final Identifier ID = Identifier.of("switchy_puffish_skills", "pufferfish_skills");
+    protected NbtCompound skillsData = new NbtCompound();
+
+    @Override
+    public NbtCompound toNbt() {
+        NbtCompound outNbt = new NbtCompound();
+        outNbt.put(KEY_SKILLS_DATA, skillsData);
+        return outNbt;
+    }
+
+    @Override
+    public void fillFromNbt(NbtCompound nbt) {
+        this.skillsData = nbt.getCompound(KEY_SKILLS_DATA);
+    }
 }
